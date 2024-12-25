3893a447eb4f0248c9a26be7ad8ce9725609831b
BrianOtto0830 <ottobrian021@gmail.com>
2024-12-25 10:49:46 +0700

    responsiveTest

--- src/app/(dasboard)/products/_components/form.tsx
+++ src/app/(dasboard)/products/_components/form.tsx
@@ -130,8 +130,8 @@ export default function FormProduct({ categories, product }: FormProductProps) {
 
   
   return (
-    <div>
-      <div className="grid grid-cols-2">
+    <div className="px-4 sm:px-6 lg:px-8">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
         <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
           <div className="border-b border-stroke px-6.5 py-4 dark:border-strokedark">
             <h3 className="font-medium text-black dark:text-white">
