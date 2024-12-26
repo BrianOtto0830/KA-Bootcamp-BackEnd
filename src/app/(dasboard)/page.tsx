c61392b32169f43af763cff42fe5875885811fd7
BrianOtto0830 <ottobrian021@gmail.com>
2024-12-26 14:15:14 +0700

    revalidate10

--- src/app/(dasboard)/page.tsx
+++ src/app/(dasboard)/page.tsx
@@ -2,7 +2,7 @@ import ECommerce from "@/components/Dashboard/E-commerce";
 import prisma from "@/lib/prisma";
 import { getProfit } from "@/lib/profit";
 
-export const revalidate = 0;
+export const revalidate = 10;
 
 export default async function Home() {
   //tampilkan semua category
